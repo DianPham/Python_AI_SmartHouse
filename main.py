84ea1d0feeb3d418625bdc793708992c25b1b7d8
DianPham <152237146+DianPham@users.noreply.github.com>
2024-05-18 00:44:03 +0700

    Re-config project

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import sounddevice as sd
 import numpy as np
 from scipy.io.wavfile import write
 from VoiceAssistant.voice import speak, play_sound
+import webrtcvad
+import noisereduce as nr
 
 path_to_python = "C:/Dian/SmartHouse#2/venv/Scripts/python.exe"
 path_to_init_server = "C:/Dian/SmartHouse#2/PhoWhisper_Server/init_server.py"
@@ -22,14 +24,45 @@ def detect_wake_word():
     process.wait()  
     return process.returncode == 1
 
-def record_audio(filename="audio.wav", duration=2, fs=16000):
-    """Record audio from the microphone and save it as a WAV file."""
+def record_audio(filename="audio.wav", duration=10, fs=16000):
+    """Record audio from the microphone, stop when silence is detected, and perform noise reduction."""
     print("Start recording...")
+
+    vad = webrtcvad.Vad()
+    vad.set_mode(1)  # Level of aggressiveness from 0 to 3
+
+    def callback(indata, frames, time, status):
+        """This is called (from a separate thread) for each audio block."""
+        if status:
+            print(status)
+
+    # Create a stream object
+    stream = sd.InputStream(callback=callback, samplerate=fs, channels=1, dtype='int16')
     start_time = time.time()
-    audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='float32')
-    sd.wait()  # Wait until recording is finished
-    write(filename, fs, (audio_data * 32767).astype(np.int16))  # Convert to int16 and save as WAV
+    with stream:
+        # Initialize variables
+        voiced_frames = []
+        detected_voice = False
+
+        for _ in range(int(duration * fs / 512)):  # Only loop duration seconds
+            data = stream.read(512)[0]  # Read 512 samples
+            # Check if the current frame contains speech
+            if vad.is_speech(data.tobytes(), sample_rate=fs):
+                voiced_frames.append(data)
+                detected_voice = True
+            elif detected_voice:
+                break  # Stop recording after speech stops
+
+    # Convert the list of numpy-arrays into a 1D array (flatten)
+    audio_data = np.concatenate(voiced_frames, axis=0)
+
+    # Noise reduction
+    reduced_noise_audio = nr.reduce_noise(y=audio_data, sr=fs)
+
+    # Convert to int16 and save as WAV
+    write(filename, fs, reduced_noise_audio.astype(np.int16))
     print("Finish recording. Saved as", filename, "in {:.2f} seconds.".format(time.time() - start_time))
+
     return filename
 
 def send_audio_to_server(audio_path):
